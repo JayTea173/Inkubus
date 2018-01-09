ac6c9a94dab35dc75fc3f328b6fd3c62b2858dcd
jaytea173 <jaytea173@googlemail.com>
2018-01-09 21:03:40 +0100

    Merge

--- Inkubus/Inkubus/Engine/GameWindowSettings.cs
+++ Inkubus/Inkubus/Engine/GameWindowSettings.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Inkubus.Engine.GameWindowSettings
+namespace Inkubus.Engine
 {
     static class GameWindowSettings
     {
