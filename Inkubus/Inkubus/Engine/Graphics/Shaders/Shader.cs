54c469b1414c9038bc6bcb713d13873f82159aed
jaytea173 <jaytea173@googlemail.com>
2018-01-09 20:52:10 +0100

    Shader stuff

---
+++ Inkubus/Inkubus/Engine/Graphics/Shaders/Shader.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Reflection;
+using OpenTK;
+using OpenTK.Graphics;
+using OpenTK.Graphics.OpenGL4;
+
+namespace Inkubus.Engine.Graphics.Shaders
+{
+    class Shader
+    {
+        protected int id;
+
+        public int ID { get { return id; } }
+        public Shader (string fileName, ShaderType shaderType)
+        {
+            string data = File.ReadAllText(fileName);
+            id = GL.CreateShader(shaderType);
+            GL.ShaderSource(id, data);
+            GL.CompileShader(id);
+        }
+
+    }
+}
---
+++ Inkubus/Inkubus/Engine/Graphics/Shaders/ShaderProgram.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenTK.Graphics.OpenGL4;
+
+namespace Inkubus.Engine.Graphics.Shaders
+{
+    class ShaderProgram
+    {
+        protected int id;
+        public string name;
+
+        public List<Shader> shaders;
+
+        public ShaderProgram(string name, Shader[] shaders)
+        {
+            this.name = name;
+            this.shaders = new List<Shader>(shaders);
+            id = GL.CreateProgram();
+            foreach (var shader in shaders)
+                GL.AttachShader(id, shader.ID);
+        }
+
+        public void Destroy()
+        {
+            GL.DeleteProgram(id);
+        }
+
+        public int Link()
+        {
+            GL.LinkProgram(id);
+
+            foreach (var shader in shaders)
+                GL.DetachShader(id, shader.ID);
+            return id;
+        }
+    }
+}
